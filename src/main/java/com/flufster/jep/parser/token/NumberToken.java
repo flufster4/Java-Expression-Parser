507bf46856caed7c6e44174801ea0bc88bdb7d90
Markian <markian.vrublevsky@gmail.com>
2024-04-14 22:15:01 -0500

    Removed @Nullable

--- src/main/java/com/flufster/jep/parser/token/NumberToken.java
+++ src/main/java/com/flufster/jep/parser/token/NumberToken.java
@@ -1,9 +1,8 @@
 package com.flufster.jep.parser.token;
 
 import com.flufster.jep.parser.tree.Node;
-import org.jetbrains.annotations.Nullable;
 
 public record NumberToken(Double value) implements Token {
     @Override
-    public void execute(@Nullable Node left, @Nullable Node right) {}
+    public void execute(Node left, Node right) {}
 }
--- src/main/java/com/flufster/jep/parser/token/Token.java
+++ src/main/java/com/flufster/jep/parser/token/Token.java
@@ -1,11 +1,10 @@
 package com.flufster.jep.parser.token;
 
 import com.flufster.jep.parser.tree.Node;
-import org.jetbrains.annotations.Nullable;
 
 public interface Token {
 
-    void execute(@Nullable Node left, @Nullable Node right);
+    void execute(Node left, Node right);
 
     Double value();
 }
