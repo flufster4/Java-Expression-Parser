c2b09e86fb1c72208c8602c2a170817b48694b43
Markian <markian.vrublevsky@gmail.com>
2024-04-15 21:33:47 -0500

    Made trig functions in radians

--- src/main/java/com/flufster/jep/math/advanced/TrigSolver.java
+++ src/main/java/com/flufster/jep/math/advanced/TrigSolver.java
@@ -9,12 +9,12 @@ public class TrigSolver implements AdvancedOperationSolver {
     @Override
     public Optional<Double> evaluate(String operation, Double operand) {
         return switch (operation) {
-            case "COS" -> Optional.of(Math.cos(Math.toRadians(operand)));
-            case "SIN" -> Optional.of(Math.sin(Math.toRadians(operand)));
-            case "TAN" -> Optional.of(Math.tan(Math.toRadians(operand)));
-            case "ARCCOS" -> Optional.of(Math.acos(Math.toRadians(operand)));
-            case "ARCSIN" -> Optional.of(Math.asin(Math.toRadians(operand)));
-            case "ARCTAN" -> Optional.of(Math.atan(Math.toRadians(operand)));
+            case "COS" -> Optional.of(Math.cos(operand));
+            case "SIN" -> Optional.of(Math.sin(operand));
+            case "TAN" -> Optional.of(Math.tan(operand));
+            case "ARCCOS" -> Optional.of(Math.acos(operand));
+            case "ARCSIN" -> Optional.of(Math.asin(operand));
+            case "ARCTAN" -> Optional.of(Math.atan(operand));
             default -> Optional.empty();
         };
     }
